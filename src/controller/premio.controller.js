55b4fd33658698078ff740cbd0d17faa3afe5d92
kpalma <kpalma@solucionesun.com>
2022-12-30 15:41:40 -0600

    add premio.js premio.controller premio.route

---
+++ src/controller/premio.controller.js
@@ -0,0 +1,112 @@
+const { Premio } = require('../models/transaccion')
+
+
+//controllador paa obtener la lista de transacciones
+const GetPremios = async (req, res) => {
+    try {
+        const trx = await Transaccion.findAll({
+            where: {
+                estado: 1
+            }
+        })
+        res.json(trx)
+    } catch (error) {
+        res.status(403)
+        res.send({ errors: 'Ha sucedido un  error al intentar agregar el premio.' });
+    }
+
+}
+
+
+//controllador para agregar nuevos Premios
+const AddPremio = async (req, res) => {
+
+    try {
+        const { descripcion, nombre, tipo, link, claveSecreta, idTransaccion } = req.body;
+        await Premio.create({
+            descripcion,
+            nombre,
+            tipo,
+            link,
+            claveSecreta,
+            idTransaccion
+        })
+        res.json({ code: 'ok', message: 'Premio creado con exito' });
+
+    } catch (error) {
+        res.status(403)
+        res.send({ errors: 'Ha sucedido un  error al intentar agregar el premio.' });
+    }
+
+}
+
+
+//controllador para actualizar los premios
+const UpdatePremio = async (req, res) => {
+
+    try {
+        const { descripcion, nombre, tipo, link, claveSecreta, idTransaccion} = req.body;
+        const {id} = req.params
+        await Premio.update({
+            descripcion,
+            nombre,
+            tipo,
+            link,
+            claveSecreta,
+            idTransaccion
+        }, {
+            where: {
+                id: id
+            }
+        });
+
+
+        res.json({ code: 'ok', message: 'Premio actualizado con exito' });
+
+    } catch (error) {
+        res.status(403)
+        res.send({ errors: 'Ha sucedido un  error al intentar agregar el premio.' });
+    }
+
+}
+
+
+//controllador para actualizar transacciones
+const DeletePremio = async (req, res) => {
+
+    try {
+        const {id} = req.params
+        await Premio.update({
+            estado : 0
+        }, {
+            where: {
+                id: id
+            }
+        });
+
+
+        res.json({ code: 'ok', message: 'Premio inhabilitado con exito' });
+
+    } catch (error) {
+        res.status(403)
+        res.send({ errors: 'Ha sucedido un  error al intentar realizar el premio.' });
+    }
+
+}
+
+
+const GetPremioById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const project = await Transaccion.findByPk(id);
+        res.json(project)
+    } catch (error) {
+        console.log(error)
+        res.status(403)
+        res.send({ errors: 'Ha sucedido un  error al intentar realizar el premio.' });
+    }
+
+}
+
+
+module.exports = { GetPremios, AddPremio, UpdatePremio, DeletePremio, GetPremioById }
---
+++ src/models/premio.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../database/database');
+
+
+
+const Premio = sequelize.define('premio', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    descripcion: {
+        type: DataTypes.STRING(150),
+        allowNull: false
+    },
+    nombre: {
+        type: DataTypes.STRING(150),
+        allowNull: false
+    },
+    tipo: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    link: {
+        type: DataTypes.STRING(500),
+        allowNull:false
+    },
+    claveSecreta: {
+        type: DataTypes.STRING(500),
+        allowNull:false
+    },
+    estado : {
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+        allowNull:false
+    },
+    idTransaccion: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    }
+
+},{timestamps: false});
+
+
+//(async () => {
+    //await sequelize.sync({ force: true });
+//     // Code here
+//})();
+
+module.exports = {Premio}
