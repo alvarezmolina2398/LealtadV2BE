ae64479bee7fdaf0a2378c93815f6b1765b9c7f2
kpalma <kpalma@solucionesun.com>
2023-01-03 09:21:13 -0600

    end premio

---
+++ src/routes/premio.routes.js
@@ -0,0 +1,18 @@
+const {Router} = require('express');
+const router = Router();
+const {GetPremios, AddPremio, UpdatePremio, DeletePremio, GetPremioById} = require('../controller/premio.controller')
+
+
+//declarampos nuestra constante para almacenar el path`
+const path = 'Premio';
+
+
+//rutas del proyecto
+router.get(`/${path}`, GetPremios);
+router.get(`/${path}/:id`,GetPremioById);
+router.post(`/${path}`,AddPremio);
+router.put(`/${path}/:id`,UpdatePremio);
+router.delete(`/${path}/:id`,DeletePremio);
+
+
+module.exports = router
