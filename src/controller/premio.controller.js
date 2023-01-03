dff5eeb6137a8b0c00a91909ce2f1406d6d252ae
Jarny Estiven <estiven6647@gmail.com>
2023-01-03 15:30:35 -0600

    modificacion de premio.controller

--- src/controller/premio.controller.js
+++ src/controller/premio.controller.js
@@ -22,16 +22,35 @@ const GetPremios = async (req, res) => {
 const AddPremio = async (req, res) => {
 
     try {
-        const { descripcion, nombre, tipo, link, claveSecreta, idTransaccion } = req.body;
-        await Premio.create({
-            descripcion,
-            nombre,
-            tipo,
-            link,
-            claveSecreta,
-            idTransaccion
-        })
-        res.json({ code: 'ok', message: 'Premio creado con exito' });
+
+        const{tipoTransaccion} = req.body;
+
+        if(tipoTransaccion === "1"){
+            const { descripcion, nombre, tipoTransaccion, idTransaccion } = req.body;
+            await Premio.create({
+                descripcion,
+                nombre,
+                tipo: tipoTransaccion,
+                idTransaccion
+            })
+
+            res.json({ code: 'ok', message: 'Premio creado con exito' });
+
+        } else if(tipoTransaccion === "2"){
+
+            const { descripcion, nombre, link, claveSecreta, tipoTransaccion } = req.body;
+            await Premio.create({
+                descripcion,
+                nombre,
+                link,
+                claveSecreta,
+                tipo: tipoTransaccion
+            })
+
+            res.json({ code: 'ok', message: 'Premio creado con exito' });
+        }
+        
+        
 
     } catch (error) {
         res.status(403)
@@ -45,27 +64,42 @@ const AddPremio = async (req, res) => {
 const UpdatePremio = async (req, res) => {
 
     try {
-        const { descripcion, nombre, tipo, link, claveSecreta, idTransaccion} = req.body;
-        const {id} = req.params
-        await Premio.update({
-            descripcion,
-            nombre,
-            tipo,
-            link,
-            claveSecreta,
-            idTransaccion
-        }, {
-            where: {
-                id: id
-            }
-        });
+        const{tipoTransaccion} = req.body;
+        const { id } = req.params
 
+        if(tipoTransaccion === "1"){
+            const { descripcion, nombre, tipoTransaccion, idTransaccion } = req.body;
+            await Premio.update({
+                descripcion,
+                nombre,
+                tipo: tipoTransaccion,
+                idTransaccion
+            }, {
+                where: {
+                    id:id
+                }
+            })
 
-        res.json({ code: 'ok', message: 'Premio actualizado con exito' });
+            res.json({ code: 'ok', message: 'Premio actualizado con exito' });
+
+        } else if(tipoTransaccion === "2"){
+
+            const { descripcion, link, claveSecreta} = req.body;
+            await Premio.update({
+                descripcion,
+                link,
+                claveSecreta
+            }, {
+                where: {
+                    id:id
+                }
+            })
+            res.json({ code: 'ok', message: 'Premio actualizado con exito' });
+        }
 
     } catch (error) {
         res.status(403)
-        res.send({ errors: 'Ha sucedido un  error al intentar agregar el premio.' });
+        res.send({ errors: 'Ha sucedido un  error al intentar actualizar el premio.' });
     }
 
 }
