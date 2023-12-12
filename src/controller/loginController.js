807c08cfab75e1b628983056cd586ad8556e9724
bryancristopherg@gmail.com <bgarcia@solucionesun.com>
2023-12-12 15:57:50 -0600

    temp office

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,46 +1,40 @@
 const { Rol } = require("../models/rol");
 const { Usuario } = require("../models/usuario");
 
+const loggin = async (req, res) => {
+  let { username, password } = req.body;
 
-const loggin = async(req,res) => {
+  try {
+    const usuario = await Usuario.findOne({
+      include: {
+        model: Rol,
+        attributes: ["descripcion"],
+      },
+      where: {
+        username: username,
+        password: password,
+      },
+      attributes: ["nombre", "telefono", "emailNotificacion", "username"],
+    });
 
-    let {username, password} = req.body;
-
-    try {
-        
-        const usuario = await Usuario.findOne({
-            include : {
-                model: Rol,
-                attributes : ['descripcion']
-            },
-            where : {
-                username : username,
-                password : password
-            },
-            attributes: ['nombre', 'telefono', 'emailNotificacion']
-        });
-
-        if(usuario != null) 
-        {
-            
-            res.json({code: 'ok', message: 'Bienvenido '+usuario.nombre, info: usuario});
-            
-        }else
-        {
-
-            res.json({code: '01', message: 'Usuario o Contraseña Incorrecta'});
-
-        }
-        
-    } catch (error) {
-
-        console.log(error)
-        res.status(403)
-        res.send({ errors: 'Ha sucedido un  error al intentar realizar la asignacion.' });
-        
+    if (usuario != null) {
+      res.json({
+        code: "ok",
+        message: "Bienvenido " + usuario.nombre,
+        info: usuario,
+      });
+    } else {
+      res.json({ code: "01", message: "Usuario o Contraseña Incorrecta" });
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(403);
+    res.send({
+      errors: "Ha sucedido un  error al intentar realizar la asignacion.",
+    });
+  }
+};
 
 module.exports = {
-    loggin
-}
+  loggin,
+};
