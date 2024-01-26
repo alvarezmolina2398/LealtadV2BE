7001480538ecb75c9ae499a68b0e103dcfd3364f
David Orozco <dorozco@solucionesun.com>
2024-01-26 16:29:55 -0600

    Beautify.

--- src/routes/permisoUsuario.routes.js
+++ src/routes/permisoUsuario.routes.js
@@ -1,15 +1,15 @@
-const {Router} =require('express');
+const { Router } = require('express');
 const router = Router();
-const {addPermiso, getPermisos,getNoAsignados, getAsignados, deletePermisos} = require('../controllers/permisoUsuario.controller')
-const {} = require('../validators/permisoUsuario');
+const { addPermiso, getPermisos, getNoAsignados, getAsignados, deletePermisos } = require('../controllers/permisoUsuario.controller')
+const { } = require('../validators/permisoUsuario');
 const authUser = require('../middlewares/auth.js');
 
 const path = 'permisosUsuario';
 
 //rutas para metodos de departamento
-router.get(`/${path}/:username`,authUser,  getPermisos);
-router.patch(`/${path}/noAsignados`,authUser,  getNoAsignados);
-router.patch(`/${path}/asignados`,  getAsignados);
+router.get(`/${path}/:username`, authUser, getPermisos);
+router.patch(`/${path}/noAsignados`, authUser, getNoAsignados);
+router.patch(`/${path}/asignados`, getAsignados);
 router.post(`/${path}`, addPermiso);
 //router.put(`/${path}/:id`,validateCreate,UpdateDepartamento);
 router.delete(`/${path}`, deletePermisos);
