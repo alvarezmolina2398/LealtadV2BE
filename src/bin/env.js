e18c5f0891f9670331797dac98b5a9978d88b5b4
David Orozco <dorozco@solucionesun.com>
2024-02-07 14:30:25 -0600

    Security config added to env.

--- src/bin/env.js
+++ src/bin/env.js
@@ -1,9 +1,12 @@
 const env = {
     jwt: {
-        secret: 'Ks7E6D2xB%c#S%PL*W#W@c1RHCaqD@vhwgLealtadV2'
+        algo: 'HS256',
+        exp: '15m',
+        secret: 'Ks7E6D2xB%c#S%PL*W#W@c1RHCaqD@vhwgLealtadV2',
     },
     bcrypt: {
-        key: 'Ky706D2xB%c#S%PL*W#W@c1RHCaqD@vhwgLealtadV2'
+        key: 'Ky706D2xB%c#S%PL*W#W@c1RHCaqD@vhwgLealtadV2',
+        sr: 10
     }
 }
 
