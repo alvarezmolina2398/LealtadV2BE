885d8f0e91a70aab8023e41e33624a83aab1fcf4
David Orozco <dorozco@solucionesun.com>
2024-02-20 09:20:57 -0600

    CampanaPremiosRetornarRandom, CampanaPremiosRetornarTodas

--- src/controllers/apiPremio.controller.js
+++ src/controllers/apiPremio.controller.js
@@ -28,19 +28,61 @@ const CampanasActualesActivas = async (idUsuario = 0, soloMostrar = 0) => {
                     REPLACE(REPLACE(nombreCampana,'&#34;','\"'),'&#39;','\''))) descripcionNotificacion, iconoAkisi,
                     imagenPush, descripcionCampana, 0 AS maximoDiario, limiteParticipaciones,
                         (SELECT COUNT(*) FROM genesis.participante_campana
-                        WHERE idUsuarioParticipante = $idUsuario
+                        WHERE idUsuarioParticipante = :idUsuario
                             AND participante_campana.idCampana = enc_campana.idCampana)
                         AS actualParticipaciones 
                 FROM genesis.enc_campana 
                 WHERE estado = 1 AND fechaFinal >= CAST(now() as date) AND fechaInicio <= CAST(now() as date)`
-            , { type: QueryTypes.SELECT });
+            , { type: QueryTypes.SELECT, replacements: { idUsuario } });
 
     }
 }
 
+const CampanaPremiosRetornarRandom = async (idCampana, idCampanaRegion) => {
+
+    await sequelize.query(`SELECT p.idTransaccion, link, claveSecreta, dcp.valor,
+                            t.descripcion, p.idPremio idtablaPremios, dppr.idPremioPR
+                            FROM genesis.det_campana_premios dcp
+                            INNER JOIN genesis.det_campana_premios_porcentaje_regiones dppr
+                                ON dppr.idCampanaPremio = dcp.idCampanaPremio
+                                AND dppr.estado = 1
+                                AND limite > 0
+                                AND dppr.limite > dppr.listo
+                                AND dppr.idCampanaRegion = :idCampanaRegion
+                            INNER JOIN genesis.premios p
+                                ON p.idPremio = dcp.idPremio
+                            LEFT JOIN genesis.transacciones t
+                                ON p.idTransaccion = t.idTransaccion
+                            WHERE idCampana = :idCampana
+                                AND dcp.estado = 1 ORDER BY RAND() LIMIT 1;`, { type: QueryTypes.SELECT, replacements: { idCampana, idCampanaRegion } });
+
+}
+
+const CampanaPremiosRetornarTodas = async (idCampana, idCampanaRegion) => {
+
+    await sequelize.query(`SELECT p.idTransaccion, link, claveSecreta, dcp.valor,
+                            t.descripcion, p.idPremio idtablaPremios, dppr.idPremioPR
+                                FROM genesis.det_campana_premios dcp
+                                INNER JOIN genesis.det_campana_premios_porcentaje_regiones dppr
+                                    ON dppr.idCampanaPremio = dcp.idCampanaPremio
+                                    AND dppr.estado = 1
+                                    AND limite > 0
+                                    AND dppr.limite > dppr.listo
+                                    AND dppr.idCampanaRegion = :idCampanaRegion
+                                INNER JOIN genesis.premios p
+                                    ON p.idPremio = dcp.idPremio
+                                LEFT JOIN genesis.transacciones t
+                                    ON p.idTransaccion = t.idTransaccion
+                                WHERE idCampana = :idCampana
+                                    AND dcp.estado = 1;`, { type: QueryTypes.SELECT, replacements: { idCampana, idCampanaRegion } });
+
+}
+
 const CampanaPremiosInfoCliente = async (base, tabla, campoid, idTransaccion) => {
 
-        await sequelize.query(`SELECT telno, department, municipality FROM pronet.tbl_customer WHERE customer_id = :idTransaccion;`, { type: QueryTypes.SELECT, replacements: { idTransaccion } });
+    await sequelize.query(`SELECT telno, department, municipality
+                                    FROM pronet.tbl_customer
+                                WHERE customer_id = :idTransaccion;`, { type: QueryTypes.SELECT, replacements: { idTransaccion } });
 
 }
 
