17043b7ffbc0fc3b63763598d2d3fcf81b18ca4a
Estiven Alvarez <estiven6647mail.com>
2024-03-21 23:25:56 -0600

    corecciones de erores

--- src/database/database.js
+++ src/database/database.js
@@ -2,8 +2,19 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("lealtadV2", "DesaSolUn", "SolUn123", {
   //host: '192.168.1.100',
-    host: "34.16.37.157",
+    host: "34.71.45.136",
   dialect: "mysql",
 });
 
-module.exports = { sequelize };
+const pronet = new Sequelize(
+  "pronet",
+  "devusr",
+  "efHBxdcV", 
+  {
+    host: "172.16.50.27",
+    port: "3306",
+    dialect: "mysql",
+  }
+)
+
+module.exports = { sequelize, pronet };
