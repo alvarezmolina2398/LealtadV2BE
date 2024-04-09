febef76121c888ba1b067cfcaef90223f1b22067
Anthony <arredondothony@gmail.com>
2024-04-09 14:27:28 -0600

    "Reporte Referidos"

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,21 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("lealtadV2", "DesaSolUn", "SolUn123", {
-  //host: '192.168.1.100',
-<<<<<<< HEAD
-    host: "34.170.217.35",
-=======
-  host: "34.170.217.35",
->>>>>>> feature-tablaDB
+const sequelize = new Sequelize(
+  "lealtadV2",
+  "DesaSolUn",
+  "SolUn123",
+  //   "root",
+  // "admin",
+  {
+    host: "34.70.236.120",
+    // host: 'localhost',
+    dialect: "mysql",
+  }
+);
+
+const pronet = new Sequelize("pronet", "devusr", "efHBxdcV", {
+  host: "172.16.50.27",
+  port: "3306",
   dialect: "mysql",
 });
 
