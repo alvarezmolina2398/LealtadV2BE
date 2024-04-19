2b84eda836cf2bf18a6c19a7baf8bfc7dd8d3a61
elenagaal <129307850+elenagaal@users.noreply.github.com>
2024-04-19 13:30:25 -0600

    host

--- src/database/database.js
+++ src/database/database.js
@@ -1,25 +1,28 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("lealtadV2", "DesaSolUn", "SolUn123", {
-  //host: '192.168.1.100',
-  host: "35.223.201.149",
-  dialect: "mysql",
-});
-
-// const sequelize = new Sequelize("lealtadV2", "root", "B@rilhas2003", {
-//   //host: '192.168.1.100',
-//   host: "localhost",
-//   dialect: "mysql",
-// });
-
-const pronet = new Sequelize(
-    "pronet",
-    "devusr",
-    "efHBxdcV", {
+const sequelize = new Sequelize(
+    "lealtadV2",
+    "DesaSolUn",
+    "SolUn123",
+    //   "root",
+    // "admin",
+    {
         host: "35.223.201.149",
-        port: "3306",
+        // host: 'localhost',
         dialect: "mysql",
     }
-)
+);
 
-module.exports = { sequelize, pronet };
+const pronet = new Sequelize("pronet", "devusr", "efHBxdcV", {
+    host: "172.16.50.27",
+    port: "3306",
+    dialect: "mysql",
+});
+
+const genesis = new Sequelize("genesis", "devusr", "efHBxdcV", {
+    host: "172.16.50.27",
+    port: "3306",
+    dialect: "mysql",
+});
+
+module.exports = { sequelize, pronet, genesis };
