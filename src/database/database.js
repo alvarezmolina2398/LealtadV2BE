1ff78d89af49ba0c599af7f2c2f500aef290769c
Sofia <asofimp@gmail.com>
2024-04-23 10:03:53 -0600

    Cambios preliminares

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("lealtadV2", "DesaSolUn", "SolUn123", {
   //host: '192.168.1.100',
   host: "34.28.190.181",
-  dialect: "mysql",
+  dialect: "mysql", 
 });
 
 // const sequelize = new Sequelize("lealtadV2", "root", "B@rilhas2003", {
