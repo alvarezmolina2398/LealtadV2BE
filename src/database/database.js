0efbb12fa127b471ec4aaa65ee6332290dde8fbd
egalicia <129307850+elenagaal@users.noreply.github.com>
2024-04-30 10:33:36 -0600

    cambio de host

--- src/database/database.js
+++ src/database/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     //   "root",
     // "admin",
     {
-        host: "35.188.213.220",
+        host: "34.30.196.159",
 
         // host: 'localhost',
         dialect: "mysql",
