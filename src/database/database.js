caab823e72791691c6885948214aba0f2269d440
ksalazar <asalazar@solucionesun.com>
2024-05-07 15:03:46 -0600

    transacciones

--- src/database/database.js
+++ src/database/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     //   "root",
     // "admin",
     {
-        host: "34.41.150.90",
+        host: "34.30.196.159",
 
         // host: 'localhost',
         dialect: "mysql",
