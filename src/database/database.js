c461972357138b869843d8ee19e5e77f9c13a66a
Sofia <asofimp@gmail.com>
2024-05-08 15:13:41 -0600

    cambios

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 
 
-// const sequelize = new Sequelize("lealtadV2", "DesaSolUn", "SolUn123", {
-//   //host: '192.168.1.100',
-//   host: "34.41.150.90",
-//   dialect: "mysql", 
-// });
+const sequelize = new Sequelize("lealtadV2", "DesaSolUn", "SolUn123", {
+  //host: '192.168.1.100',
+  host: "34.136.61.24",
+  dialect: "mysql", 
+});
 
 // const sequelize = new Sequelize("lealtadV2", "root", "B@rilhas2003", {
 //   //host: '192.168.1.100',
@@ -13,11 +13,11 @@ const { Sequelize } = require("sequelize");
 //   dialect: "mysql",
 // });
 
-const sequelize = new Sequelize("lealtadv2", "asofi", "Pruebas2024", {
-  //host: '192.168.1.100',
-  host: "127.0.0.1",
-  dialect: "mysql",
-});
+// const sequelize = new Sequelize("lealtadv2", "asofi", "Pruebas2024", {
+//   //host: '192.168.1.100',
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+// });
 
 
 const pronet = new Sequelize(
