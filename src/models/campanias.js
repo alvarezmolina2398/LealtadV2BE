7090a5db994abbf39108f2d5bc30931ae26da182
elenagaal <129307850+elenagaal@users.noreply.github.com>
2024-05-14 15:46:51 -0600

    correcciones

--- src/models/campanias.js
+++ src/models/campanias.js
@@ -4,15 +4,10 @@ const { Bloqueados } = require('./bloqueados');
 const { Etapa } = require('./etapa');
 const { Participacion } = require('./Participacion');
 const { Participantes } = require('./participantes');
-<<<<<<< HEAD
-const { Parametro } = require('./parametro');
-// const {Usuario} = require('./usuario');
-=======
 const {Usuario} = require('./usuario');
 const {Parametro} = require('./parametro');
 // const { Configuraciones } = require('./configuraciones');
 
->>>>>>> 85f30aebd3211b38996ff48b383ba0bbe9a7653b
 
 const Campania = sequelize.define('campania', {
     id: {
