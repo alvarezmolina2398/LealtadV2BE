6a314c1fe031be5411449561cf7da667ee39017a
kenedy <kenedyhtml@gmail.com>
2024-05-27 22:58:58 -0600

    temp

---
+++ src/routes/participantes.routes.js
@@ -0,0 +1,22 @@
+const {Router} = require('express');
+const router = Router();
+const {GetTransaccions, getParticipantes,} = require('../controllers/participantes.controller.js')
+const {validateCreate} = require('../validators/transaccion')
+const authUser = require('../middlewares/auth.js');
+
+
+//declarampos nuestra constante para almacenar el path`
+const path = 'Participante';
+//rutas del proyecto
+router.get(`/${path}`,authUser, getParticipantes);
+// router.get(`/${path}/count`,authUser, GetTransaccionscount);
+// router.get(`/${path}/:id`,authUser, GetTransaccionById);
+// router.post(`/${path}`,validateCreate,authUser, AddTransaccion);
+// router.put(`/${path}/:id`,validateCreate,authUser, UpdateTransaccion);
+// router.delete(`/${path}/:id`,authUser, DeleteTransaccion);
+
+
+
+
+
+module.exports = router
