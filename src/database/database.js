056d4334a5ebfbb7b9a39f5f0ce8f3365588748f
elenagaal <129307850+elenagaal@users.noreply.github.com>
2024-05-31 16:42:25 -0600

    hosting cambiado

--- src/database/database.js
+++ src/database/database.js
@@ -1,25 +1,44 @@
+
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("lealtadV2", "DesaSolUn", "SolUn123", {
-    //host: '192.168.1.100',
-    host: "35.193.250.241",
+
+
+const sequelize = new Sequelize(
+    "db0csmijjuvbuo",
+    "uhxm0qdfefvou",
+    "DesaSolUn",
+    {
+        host: "34.174.109.166",
+        dialect: "mysql",
+}
+);
+
+// const sequelize = new Sequelize(
+//     "lealtadV2",
+//     // "DesaSolUn",
+//     // "SolUn123",
+//       "root",
+//     "admin",
+//     {
+  
+
+//         host: 'localhost',
+//         port: "3006",
+//         dialect: "mysql",
+//     }
+// );
+
+
+const pronet = new Sequelize("pronet", "devusr", "efHBxdcV", {
+    host: "172.16.50.27",
+    port: "3306",
     dialect: "mysql",
 });
 
-// const sequelize = new Sequelize("lealtadV2", "root", "B@rilhas2003", {
-//   //host: '192.168.1.100',
-//   host: "localhost",
-//   dialect: "mysql",
-// });
+const genesis = new Sequelize("genesis", "devusr", "efHBxdcV", {
+    host: "172.16.50.27",
+    port: "3306",
+    dialect: "mysql",
+});
 
-const pronet = new Sequelize(
-    "pronet",
-    "devusr",
-    "efHBxdcV", {
-        host: "172.16.50.27",
-        port: "3306",
-        dialect: "mysql",
-    }
-)
-
-module.exports = { sequelize, pronet };
+module.exports = { sequelize, pronet, genesis };
